98f8c2e52ac903706a0e40cd5140a016cdc9f1bb
DongHyun <kimdh0315@naver.com>
2023-09-26 18:08:56 +0900

    Initial commit

---
+++ src/test/java/com/fruntier/fruntier/FruntierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fruntier.fruntier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruntierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
