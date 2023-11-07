6bd716486c5d121ccb009d56be8ebd951f754654
Seokmin Woo <clematis7593@gmail.com>
2023-11-07 11:21:02 +0900

    test(test/running/service/UserRequestServiceTest.java): Add test code for UserRequestService
    Added test code for convertCoordinateToVertex

---
+++ src/test/java/com/fruntier/fruntier/running/service/UserRequestServiceTest.java
@@ -0,0 +1,53 @@
+package com.fruntier.fruntier.running.service;
+
+import com.fruntier.fruntier.running.domain.Coordinate;
+import com.fruntier.fruntier.running.domain.Vertex;
+import com.fruntier.fruntier.running.repository.VertexMemoryRepository;
+import com.fruntier.fruntier.running.repository.VertexRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserRequestServiceTest {
+
+    private final VertexMemoryRepository vertexRepository = new VertexMemoryRepository();
+    private final UserRequestService userRequestService = new UserRequestServiceImpl(vertexRepository);
+
+    @BeforeEach
+    void beforeTest(){
+        // insertion code for vertex
+        Coordinate c1 = new Coordinate(131D, 132D);
+        Coordinate c2 = new Coordinate(133D, 135D);
+        Coordinate c3 = new Coordinate(135D, 137D);
+        Vertex v1 = new Vertex(0L, c1, null, null);
+        Vertex v2 = new Vertex(1L, c2, null, null);
+        Vertex v3 = new Vertex(2L, c3, null, null);
+        vertexRepository.save(v1);
+        vertexRepository.save(v2);
+        vertexRepository.save(v3);
+    }
+
+    @AfterEach
+    void afterTest(){
+        vertexRepository.clear();
+    }
+
+
+
+    @Test
+    void convertCoordinateToVertexTest(){
+        // given
+        Coordinate c1 = new Coordinate(131.5D, 131.5D);
+
+        // when
+        Vertex v0_0 = userRequestService.convertCoordinateToVertex(c1);
+        Vertex v0_1 = vertexRepository.findById(0L).get();
+
+        // then
+        assertThat(v0_0).isEqualTo(v0_1);
+    }
+}
