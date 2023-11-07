4cdda00a8004cb5d8e6a6b1b2f53f4a7a179d73c
Seokmin Woo <clematis7593@gmail.com>
2023-11-07 11:18:18 +0900

    feat(fruntier/running/service): add implementation for UserRequestService
    Added implmentation for UserRequestService:  UserRequestServiceImpl
    
    Changed interface UserRequestService
    - return value was void, but has to be Vertex
    - parameter was UserRequest, but has to be Coordinate

--- src/main/java/com/fruntier/fruntier/running/service/UserRequestService.java
+++ src/main/java/com/fruntier/fruntier/running/service/UserRequestService.java
@@ -1,8 +1,10 @@
 package com.fruntier.fruntier.running.service;
 
+import com.fruntier.fruntier.running.domain.Coordinate;
 import com.fruntier.fruntier.running.domain.UserRequest;
+import com.fruntier.fruntier.running.domain.Vertex;
 
 public interface UserRequestService {
-    void convertCoordinateToVertex(UserRequest userRequest);
+    Vertex convertCoordinateToVertex(Coordinate coordinate);
 
 }
