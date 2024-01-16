546d2d465ec002f9877e0f12b79b9a8c6fa26774
Seokmin Woo <clematis7593@gmail.com>
2024-01-16 13:06:29 +0900

    feature: Add Join API

--- src/main/java/com/fruntier/fruntier/user/controller/UserController.java
+++ src/main/java/com/fruntier/fruntier/user/controller/UserController.java
@@ -1,10 +1,44 @@
 package com.fruntier.fruntier.user.controller;
 
+import com.fruntier.fruntier.user.domain.User;
+import com.fruntier.fruntier.user.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private final UserInfoService userInfoService;
+
+    @Autowired
+    public UserController(UserInfoService userInfoService){
+        this.userInfoService = userInfoService;
+    }
+
+    @GetMapping("/join")
+    public String userJoinPage() {
+        return "join";
+    }
+
+    @ResponseBody
+    @PostMapping("/join")
+    public String userJoin(@RequestBody Map<String, String> joinData) {
+        if (joinData.containsValue(null)){
+            // throw exception
+        }
+        System.out.println("joinData = " + joinData);
+        String username = joinData.get("username");
+        String password = joinData.get("password");
+        String email = joinData.get("email");
+        String name = joinData.get("name");
+        String address = joinData.get("address");
+        String sex = joinData.get("male");
+
+        User user = new User();
+
+        return "ok";
+    }
 }
