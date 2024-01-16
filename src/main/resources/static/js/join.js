19aad143bc9693be3a3ad3dfad3520acc7dd5fd5
DongHyun <kimdh0315@naver.com>
2024-01-16 14:48:54 +0900

    fix : add html xmlns:th address

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -2,6 +2,7 @@ window.onload = () => {
     document.getElementById("joinSubmitButton").addEventListener("click", () => {
         let formData = new FormData(document.getElementById("joinForm"));
         let send_object = JSON.stringify(Object.fromEntries(formData));
+        console.log(send_object);
         fetch('/user/join', {
             method: 'POST',
             headers: {
@@ -12,6 +13,7 @@ window.onload = () => {
             .then(response => response.text())
             .then((data) => {
                     if (data == "ok"){
+                        console.log("OK?");
                         window.location.replace("/user/login");
                     }
                     else{
