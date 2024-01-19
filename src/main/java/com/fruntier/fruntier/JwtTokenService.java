a323c8eaf03f4ea529eb4cfe951d7dd0fc21d382
Sanity1207 <newxxenson@naver.com>
2024-01-19 13:42:09 +0900

    feat : finished working on JWT logic (log in, log out, token persistence) and fixed login.html

--- src/main/java/com/fruntier/fruntier/JwtTokenService.java
+++ src/main/java/com/fruntier/fruntier/JwtTokenService.java
@@ -1,13 +1,12 @@
 package com.fruntier.fruntier;
 import com.fruntier.fruntier.user.domain.User;
+import com.fruntier.fruntier.user.exceptions.TokenValidationException;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import javax.crypto.SecretKey;
 
@@ -36,19 +35,27 @@ public class JwtTokenService {
                 .compact();
     }
 
-    public boolean validateToken(String token) {
+    public User validateTokenReturnUser(String token) throws Exception{
         try {
-            Jws<Claims> claims = Jwts.parser()
+            Claims claims = Jwts.parser()
                     .verifyWith(key)
                     .build()
-                    .parseSignedClaims(token);
+                    .parseSignedClaims(token)
+                    .getPayload();
 
             // Additional validation can be done here (e.g., checking user ID, roles)
+            User loginUser = new User();
+            loginUser.setUsername(claims.getSubject());
+            loginUser.setId(claims.get("id",Long.class));
 
-            return true; // Token is valid
-        } catch (ExpiredJwtException | SignatureException | IllegalArgumentException e) {
-            // Handle invalid token, expired token or token with invalid signature
-            return false;
+
+            return loginUser; // Token is valid
+        } catch (ExpiredJwtException e) {
+            throw new TokenValidationException("Token is expired", e);
+        } catch (SignatureException e) {
+            throw new TokenValidationException("Invalid token signature", e);
+        } catch (IllegalArgumentException e) {
+            throw new TokenValidationException("Token is invalid", e);
         }
     }
 }
