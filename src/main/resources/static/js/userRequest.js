961b30472d5a215c65a2de73723d55325a498b31
DongHyun <kimdh0315@naver.com>
2024-02-03 14:00:53 +0900

    refactor: devide methods per functions and UI

--- src/main/resources/static/js/userRequest.js
+++ src/main/resources/static/js/userRequest.js
@@ -1,207 +1,186 @@
-var markers = []; //markers displayed on map
-var vertexList = [];//vertices sending to java
-var startVertex;//start vertex user selects
-var endVertex;//end vertex user selects
-var latlng;//lat and lng info
-var expected_distance; //expected distance user input
+/* global variables */
+var markerLatLng;
+var vertexMap = [];
+var result = [];
+
 window.onload = function () {
-    var startCoordinatesElement = document.getElementById('startCoordinates');
-    var endCoordinatesElement = document.getElementById('endCoordinates');
-    var allCoordinatesElement = document.getElementById('allCoordinates');
-    var expectedDistanceElement = document.getElementById('exDistance');
-    var showReceivedRouteElement = document.getElementById('showReceivedRoute');
+    /* KaKao Map Container*/
     var mapContainer = document.getElementById('map');
     var mapOptions = {
-        center: new kakao.maps.LatLng(37.5509442, 126.9410023),//initially sogang university
-        level: 5
+        center : new kakao.maps.LatLng(37.5509442, 126.9410023), //at Sogang University
+        level : 5 //map closing level
     };
-    //map container
     var map = new kakao.maps.Map(mapContainer, mapOptions);
-    //marker container
-    var marker = new kakao.maps.Marker({
-        map: map,
+
+    /* Marker Container */
+    var markerOptions = {
+        map : map
+    };
+    var marker = new kakao.maps.Marker(markerOptions);
+
+
+    /* Mouse Click Event on the Map */
+    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+        markerLatLng = mouseEvent.latLng;
+        marker.setPosition(markerLatLng);
+        var coordinateMessage = "Coordination => Lat : " + markerLatLng.getLat() + " Lng : " + markerLatLng.getLng();
+        console.log(coordinateMessage);
     });
 
-    kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
-        latlng = mouseEvent.latLng;
-        marker.setPosition(latlng);
-        var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-        message += '경도는 ' + latlng.getLng() + ' 입니다';
-        console.log(message);
-    });
-
-    //set start vertex
+    /* Set Start Coordinate */
     document.getElementById("setStartVertex").addEventListener('click', setStartVertex);
     function setStartVertex() {
-        startVertex = latlng;
-        updateVertexList("start", startVertex);
-        startCoordinatesElement.innerText = startVertex.getLat() + ", " + startVertex.getLng();
+        setVertexMap("start");
     }
 
-    //set end vertex
+    /* Set End Coordinate */
     document.getElementById("setEndVertex").addEventListener('click', setEndVertex);
     function setEndVertex() {
-        endVertex = latlng;
-        updateVertexList("end", endVertex);
-        endCoordinatesElement.innerText = endVertex.getLat() + ", " + endVertex.getLng();
+        setVertexMap("end");
     }
 
-    //show selected start and end vertices on the map
-    document.getElementById("vertexList").addEventListener('click', showVertex);
-    function showVertex() {
-        allCoordinatesElement.innerText = "";
-
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-        }
-        markers = [];
-
-        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-        for (var i = 0; i < vertexList.length; i++) {
-            var marker = showMarker(vertexList[i].title, vertexList[i].latlng, imageSrc);
-            markers.push(marker);
-        }
-    }
-
-    //draw marker selected start and end vertices on the map
-    function showMarker(title, latlng, imageSrc) {
-        var imageSize = new kakao.maps.Size(24, 35);
-        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: latlng,
-            title: title,
-            image: markerImage
+    /* Update Start and End Vertex Map */
+    function setVertexMap(title) {
+        var coordinateIndex = vertexMap.findIndex(function (coordinate) {
+            return coordinate.title === title;
         });
-        allCoordinatesElement.innerText += marker.getTitle() + " : " + marker.getPosition().getLat() + " , " + marker.getPosition().getLng() + "\n";
 
-        return marker;
+        if (coordinateIndex !== -1) vertexMap[coordinateIndex].latLng = markerLatLng;
+        else vertexMap.push( {"title" : title, "latLng" : markerLatLng} );
+
+        console.log(vertexMap);
     }
 
-    /* communicate with java spring. send start,end vertex information to vertexController */
-    document.getElementById("sendVertices").addEventListener('click', sendVertexToServer);
-    function sendVertexToServer() {
+    /* Set Expected Distance */
+    function getExpectedDistance() {
         var expectedDistance = document.getElementById("expectedDistance").value;
+        if (!Number.isInteger(parseInt(expectedDistance))) {
+            alert("Error: 뛰고자하는 거리를 입력해주세요.");
+            return -1;
+        }
+        return expectedDistance;
+    }
 
-        // Ensure integerValue is a valid integer (you may want to add additional validation)
-        if(!Number.isInteger(parseInt(expectedDistance))) {
-            alert("error number");
+    /* Request the route to the Server */
+    document.getElementById("generateRoute").addEventListener('click', requestServerGenerateRoute);
+    function requestServerGenerateRoute() {
+        var expectedDistance = getExpectedDistance();
+
+        if (validateUserForm(expectedDistance) === false) {
+            console.log(vertexMap);
+            alert("Error: 모든 정보를 세팅해주세요.");
             return;
         }
 
+        var sendingData = makeSendingDataJSON(expectedDistance);
 
-        var vertices = JSON.stringify({
-            expectedDistance: parseInt(expectedDistance),
-            vertices: vertexList.map(item => ({ title: item.title, coordinate: { latitude: item.latlng.getLat(), longitude: item.latlng.getLng() } }))
+        var serverContainer = openConnectionServer();
+
+        communicateServer(serverContainer, sendingData, function (error, response) {
+            if (error) {
+                alert("Error: 서버 통신 중 오류가 발생했습니다.");
+                return;
+            }
+
+            if (validateRecommendRoute(response) === false) {
+                alert("Error: 추천 경로 생성 실패");
+                return;
+            }
+
+            drawRouteOnMap(response);
         });
+    }
 
-        console.log('Vertices:', vertices);
+    function validateUserForm(expectedDistance) {
+        //not exist expected distance
+        if (expectedDistance === -1) return false;
+
+        const hasStartVertex = Array.from(vertexMap.values()).some(value => value.title === "start");
+        const hasEndVertex = Array.from(vertexMap.values()).some(value => value.title === "end");
+
+        if (!hasStartVertex || !hasEndVertex) return false;
+
+        return true;
+    }
+
+    function makeSendingDataJSON(expectedDistance) {
+        var data = JSON.stringify({
+            expectedDistance: parseInt(expectedDistance),
+            vertices: vertexMap.map(function (item) {
+                return {
+                    title : item.title,
+                    coordinate : {latitude : item.latLng.getLat(), longitude : item.latLng.getLng() }
+                };
+            })
+        })
+
+        return data;
+    }
+
+    function openConnectionServer() {
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '/api/routes/recommendation', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    var response = JSON.parse(xhr.responseText);
-                    console.log("response : ", response);
-                    if (response != null) {
-                        drawLineOnMap(response);
-                    } else {
-                        console.log('Server response:', response);
-                    }
+        return xhr;
+    }
+
+    function communicateServer(server, sendingData, callback) {
+        var response;
+
+        server.onreadystatechange = function() {
+            if (server.readyState === 4) {
+                if (server.status === 200) {
+                    response = JSON.parse(server.responseText);
+                    callback(null, response);
+                    console.log("Server Response : ", response);
                 } else {
-                    console.error('Error:', xhr.status, xhr.statusText, xhr.responseText);
+                    console.log("Error : ", server.status, server.statusText, server.responseText);
+                    callback(new Error(`Server error: ${server.status}`), null);
                 }
             }
         };
 
-        xhr.send(vertices);
+        server.send(sendingData);
     }
 
-    //vertexList에 start, end vertex를 저장. 이미 있다면 replace
-    function updateVertexList(title, latlng) {
-        var existingItemIndex = vertexList.findIndex(item => item.title === title);
-        if (existingItemIndex !== -1) {
-            vertexList[existingItemIndex].latlng = latlng;
-        } else {
-            vertexList.push({ "title": title, "latlng": latlng });
-        }
+    function validateRecommendRoute(recommendRoute) {
+        return true;
     }
 
-//    document.getElementById("showRoute").addEventListener('click', showReceivedRoute);
-//    function showReceivedRoute() {
-//        //make an HTTP request to the backend
-//        fetch('/api/get-data')
-//            .then(response => {
-//                if (!response.ok) {
-//                    throw new Error('Network response was not ok');
-//                }
-//                return response.json();
-//            })
-//            .then(data => {
-//                //Handle the JSON data
-//                console.log('Received data:', data);
-//
-//                //console.log data
-//                console.log('Key:', data.key);
-//                console.log('Value:', data.value);
-//
-//                //drawMarkerOnMap(data);
-//                drawLineOnMap(data);
-//            })
-//            .catch(error => console.error('Error:', error));
-//    }
+    function drawRouteOnMap(recommendRoute) {
+        //clear the markers
+        result.forEach(function(m) {
+            m.setMap(null);
+        });
+        result = [];
 
-//    function drawMarkerOnMap(data) {
-//        console.log("in draw:",data);
-//
-//        //draw markers on the Kakao Map using data
-//        data.forEach(item => {
-//            console.log("create marker :",item.latlng.lat, item.latlng.lng);
-//                map: map,
-//                position: new kakao.maps.LatLng(item.latlng.lat, item.latlng.lng),
-//            });
-//            markers.push(marker);
-//        });
-//    }
-
-    function drawLineOnMap(data) {
-        //clear existing markers and lines
-        markers.forEach(marker => marker.setMap(null));
-        markers = [];
-
-        //draw markers and lines
-        for (var i = 0; i < data.length - 1; i++) {
+        for (var i = 0; i < recommendRoute.length - 1; i++) {
             var currentMarker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(data[i].coordinate.latitude, data[i].coordinate.longitude),
-                title: data[i].title,
+                map : map,
+                position : new kakao.maps.LatLng(recommendRoute[i].coordinate.latitude, recommendRoute[i].coordinate.longitude),
+                title : recommendRoute[i].title,
             });
 
             var nextMarker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(data[i + 1].coordinate.latitude, data[i + 1].coordinate.longitude),
-                title: data[i + 1].title,
+                map : map,
+                position : new kakao.maps.LatLng(recommendRoute[i+1].coordinate.latitude, recommendRoute[i+1].coordinate.longitude),
+                title : recommendRoute[i+1].title,
             });
 
-            // Draw a line between consecutive markers
             var linePath = [currentMarker.getPosition(), nextMarker.getPosition()];
             var polyline = new kakao.maps.Polyline({
-                path: linePath,
-                strokeWeight: 3,
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.7,
-                strokeStyle: 'solid',
+                path : linePath,
+                strokeWeight : 3,
+                strokeColor : '#FF0000',
+                strokeOpacity : 0.7,
+                strokeStyle : 'solid',
             });
 
-            // Set the polyline on the map
             polyline.setMap(map);
-
-            // Save markers and polyline to the markers array
-            markers.push(currentMarker);
-            markers.push(nextMarker);
-            markers.push(polyline);
+            result.push(currentMarker);
+            result.push(nextMarker);
+            result.push(polyline);
         }
     }
-};
+}
